da45b77633ce9c1f193225360bd0900c122fe58c
Prayaan1711 <spyfyi1711@gmail.com>
2025-04-19 09:37:23 +0530

    1234

---
+++ And and or.py
@@ -0,0 +1,22 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All numbers are true...")
+else:
+    print("One number is false...")
+
+d = 10
+e = -10
+f = 0
+
+if d > 0 or e > 0:
+    print("One number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if e > 0 or f > 0:
+    print("One number is greater than 0")
+else:
+    print("No number is greater than 0")
